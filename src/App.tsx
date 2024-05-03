a86b3e25beca03937b5b560afa8b305e22cdcc53
Abdulmalik Almasud <mlk1321.aa@gmail.com>
2024-05-03 13:34:49 +0300

    starting a new branch for old portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log("app reloading");
+
+  setTimeout(() => {
+    const loading = document.getElementById("loading") as HTMLDivElement;
+    const test = document.getElementById("test") as HTMLDivElement;
+    loading.children[0].children[0].classList.add("loading-active");
+    setTimeout(() => {
+      loading.classList.add("loading__off");
+      setTimeout(() => {
+        test.classList.add("test-in");
+        setTimeout(() => {
+          test.classList.add("test-up");
+        }, 50);
+      }, 100);
+      setTimeout(() => {
+        loading.style.display = "none";
+      }, 500);
+    }, 500);
+  }, 100);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="loading" className="loading">
+        <span className="loading__span">
+          <span className="loading__span--inner"></span>
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div id="test" className="test">
+        <h3 className="text-3xl underline bg-slate-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+          autem non voluptatem facilis vero beatae dignissimos pariatur nemo
+          quos itaque eaque quasi blanditiis dolorem, accusamus sapiente et
+          animi expedita explicabo!
+        </h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
